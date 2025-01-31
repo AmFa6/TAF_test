a2348ac472ed7a5e5708d8e58e270e284162ae3b
AmFa6 <162441727+AmFa6@users.noreply.github.com>
2025-01-31 14:50:37 +0000

    Update main.js

--- main.js
+++ main.js
@@ -425,7 +425,7 @@ function toggleInverseScale() {
     });
     handles[1].classList.add('noUi-handle-transparent');
     handles[0].classList.remove('noUi-handle-transparent');
-    connectElements[1].style.background = 'linear-gradient(to left, rgba(118, 118, 118, 0) 0%, rgba(118, 118, 118, 0.5) 50%, rgba(118, 118, 118, 1) 100%)'; // Dark grey to transparent
+    connectElements[0].style.background = 'linear-gradient(to left, rgba(118, 118, 118, 0) 0%, rgba(118, 118, 118, 0.5) 50%, rgba(118, 118, 118, 1) 100%)'; // Dark grey to transparent
   } else {
     opacityRangeSlider.noUiSlider.updateOptions({
       connect: [true, true, false] // Set connect to true, true, false
