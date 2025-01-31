9dfe4016a42bea8ad24279aadd55dc71c40575d0
AmFa6 <162441727+AmFa6@users.noreply.github.com>
2025-01-31 12:21:51 +0000

    Update main.js

--- main.js
+++ main.js
@@ -421,18 +421,19 @@ function toggleInverseScale() {
 
   if (isInverse) {
     opacityRangeSlider.noUiSlider.updateOptions({
-      connect: [false, true, true] // Set connect to false, true, true
+      connect: [true, true, false] // Set connect to false, true, true
     });
     handles[1].classList.add('noUi-handle-transparent');
     handles[0].classList.remove('noUi-handle-transparent');
-    connectElements[0].style.background = 'linear-gradient(to left, rgba(118, 118, 118, 0) 0%, rgba(118, 118, 118, 0.5) 50%, rgba(118, 118, 118, 1) 100%)'; // Dark grey to transparent
+    connectElements[0].style.background = 'noUi-connect-dark-slider';
+    connectElements[1].style.background = 'linear-gradient(to left, rgba(118, 118, 118, 0) 0%, rgba(118, 118, 118, 0.5) 50%, rgba(118, 118, 118, 1) 100%)';
   } else {
     opacityRangeSlider.noUiSlider.updateOptions({
-      connect: [true, true, false] // Set connect to true, true, false
+      connect: [false, true, true] // Set connect to true, true, false
     });
     handles[1].classList.remove('noUi-handle-transparent');
     handles[0].classList.add('noUi-handle-transparent');
-    connectElements[0].style.background = 'linear-gradient(to right, rgba(118, 118, 118, 0) 0%, rgba(118, 118, 118, 0.5) 50%, rgba(118, 118, 118, 1) 100%)'; // Transparent to dark grey
+    connectElements[0].style.background = 'linear-gradient(to right, rgba(118, 118, 118, 0) 0%, rgba(118, 118, 118, 0.5) 50%, rgba(118, 118, 118, 1) 100%)';
   }
   updateLayerVisibility();
 }
