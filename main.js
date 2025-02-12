3da8ed616844535167f883d534bf00f0fe4a7854
AmFa6 <162441727+AmFa6@users.noreply.github.com>
2025-02-12 11:34:20 +0000

    Update main.js

--- main.js
+++ main.js
@@ -233,7 +233,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 map.on('zoomend', () => {
   console.log("Zoom end event triggered");
-  drawSelectedAmenities([]);
+  drawSelectedAmenities();
 });
 
 function initializeSliders(sliderElement, updateCallback) {
