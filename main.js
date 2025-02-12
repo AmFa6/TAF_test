278ac71bf7f87d7bfba31a405fc3a7dca9da757d
AmFa6 <162441727+AmFa6@users.noreply.github.com>
2025-02-12 11:31:51 +0000

    Update main.js

--- main.js
+++ main.js
@@ -233,7 +233,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 map.on('zoomend', () => {
   console.log("Zoom end event triggered");
-  updateAmenitiesVisibility();
+  drawSelectedAmenities([]);
 });
 
 function initializeSliders(sliderElement, updateCallback) {
@@ -578,40 +578,6 @@ function drawSelectedAmenities(selectedAmenities) {
       amenitiesLayerGroup.addLayer(layer);
     }
   });
-
-  updateAmenitiesVisibility();
-}
-
-function updateAmenitiesVisibility() {
-  console.log("updateAmenitiesVisibility called");
-  const currentZoom = map.getZoom();
-  const minZoomLevel = 14;
-  console.log("Current zoom level in updateAmenitiesVisibility:", currentZoom);
-
-  amenitiesLayerGroup.eachLayer(layer => {
-    layer.eachLayer(marker => {
-      const amenity = Object.keys(amenityIcons).find(key => {
-        const iconHtml = marker.options.icon.options.html;
-        return iconHtml && iconHtml.includes(amenityIcons[key].options.html);
-      });
-
-      if (currentZoom >= minZoomLevel) {
-        console.log(`Setting icon to pin for amenity: ${amenity}`);
-        console.log(`Current icon HTML: ${marker.options.icon.options.html}`);
-        console.log(`New icon HTML: ${amenityIcons[amenity].options.html}`);
-        marker.setIcon(amenityIcons[amenity]);
-      } else {
-        console.log(`Setting icon to point for amenity: ${amenity}`);
-        console.log(`Current icon HTML: ${marker.options.icon.options.html}`);
-        console.log(`New icon HTML: <div class="dot"></div>`);
-        marker.setIcon(L.divIcon({ className: 'fa-icon', html: '<div class="dot"></div>', iconSize: [10, 10], iconAnchor: [5, 5] }));
-      }
-    });
-  });
-
-  if (!map.hasLayer(amenitiesLayerGroup)) {
-    amenitiesLayerGroup.addTo(map);
-  }
 }
 
 function AmenitiesPopup(amenity, properties) {
